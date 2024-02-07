9ae13cc9f59c6576f4a365f66ada2b178a6e0c0b
Wiilly888 <105108719+Wiilly888@users.noreply.github.com>
2024-02-07 10:48:44 +0100

    Update Template.java

--- src/main/java/Wiilly/Template/common/Template.java
+++ src/main/java/Wiilly/Template/common/Template.java
@@ -9,7 +9,9 @@ public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+	public static final String MOD_ID = "Template_id";
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize(ModContainer mod) {
